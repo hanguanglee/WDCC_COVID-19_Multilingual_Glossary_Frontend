f77d2dbbade0f8e3bc4ea3f7240ca2df82e483bd
Kevin Ge <gjw1047340859@gmail.com>
2021-05-27 14:03:29 +1200

    fix start seach button css

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -3,20 +3,25 @@ import "../css/WelcomePage.css";
 import { Link } from "react-router-dom";
 
 class WelcomePage extends React.Component {
-    render() {
-        return (
-            <div className={ ` welcomePage ${ this.props.darkMode ? "dark-mode" : "" } ` }>
-                <h1 id="title-txt">COVID-19 Multilingual Glossary</h1>
-                <br />
-                <Link to="/translations">
-                    <button type="button" className={ ` btn btn-outline-dark 
-                    ${ this.props.darkMode? "btn-dark-mode" : "" } ` }>Start searching</button>
-                </Link>
-            </div>
-
-        );
-    }
-
+  render() {
+    return (
+      <div
+        className={` welcomePage ${this.props.darkMode ? "dark-mode" : ""} `}
+      >
+        <h1 id="title-txt">COVID-19 Multilingual Glossary</h1>
+        <br />
+        <Link to="/translations">
+          <button
+            type="button"
+            className={` btn welcomeBtn btn-outline-dark 
+                    ${this.props.darkMode ? "btn-dark-mode" : ""} `}
+          >
+            Start searching
+          </button>
+        </Link>
+      </div>
+    );
+  }
 }
 
 export default WelcomePage;
